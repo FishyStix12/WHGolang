e0fb7b08a6f1dcff0c3c6bb9cafd637a451f5dc9
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-03-18 23:36:48 -0500

    Update goconfig.sh

--- goconfig.sh
+++ goconfig.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# First, install the package
+# First, install the go file for Linux at  https://go.dev/dl/
 sudo apt install -y golang
 
 # Then add the following to your .bashrc
@@ -8,4 +8,4 @@ export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
 # Reload your .bashrc
-source .bashrc
+source ~/.bashrc
