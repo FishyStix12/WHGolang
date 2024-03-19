ddec02ecbb51823610abba622cc6c3375dcf0c79
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-03-18 23:46:50 -0500

    Create hello_world.go

---
+++ hello_world.go
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Author: Nicholas Fisher
+// Date: March 18th 2024
+// Description of the Script
+// The script is a simple Go program that demonstrates the basic structure of a Go application. It starts
+// by declaring that it belongs to the `main` package, imports the "fmt" package for formatting input and output,
+// defines the `main` function as the entry point of the program, and then prints "Hello, Black Hat Gophers!" 
+// to the console using the `fmt.Println` function. This script serves as a starting point for learning
+// how to write and execute Go programs.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+package main // Declares that this is part of the 'main' package
+
+import (
+    "fmt" // Imports the 'fmt' package for formatting input and output
+)
+
+func main() { // Defines the 'main' function, which is the entry point of the program
+    fmt.Println("Hello, Black Hat Gophers!") // Prints the message "Hello, Black Hat Gophers!" to the console
+}
