a7380a7fe085353d2f4eb589e1849a90edab9e47
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-05-16 10:35:51 -0500

    Add files via upload

---
+++ testscan.go
@@ -0,0 +1,13 @@
+package main // Declares that this file belongs to the main package, which can be executed as a standalone program.
+
+import (
+	"fmt" // Importing the fmt package for printing messages to the console.
+	"net" // Importing the net package for network-related operations.
+)
+
+func main() { // Entry point of the program.
+	_, err := net.Dial("tcp", "scanme.nmap.org:80") // Attempting to establish a TCP connection to "scanme.nmap.org" on port 80.
+	if err == nil {                                 // Checking if there was no error during the connection attempt.
+		fmt.Println("Connection successful") // If no error occurred, print "Connection successful".
+	}
+}
