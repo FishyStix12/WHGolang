3664933976955a38a1c2c677854cf453cf51ae0a
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-07-21 11:44:35 -0400

    Update channels.go

--- channels.go
+++ channels.go
@@ -1,4 +1,4 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Author: Nicholas Fisher
 // Date: March 18th 2024
 // Description of the Script
@@ -6,10 +6,10 @@
 // the length of a given string and sends it to a channel. Then, in the main function, it creates a channel,
 // launches strlen in a separate goroutine, receives the length of the string from the channel, and prints
 // the sum of the lengths of the string.\
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////
 package main // Declares the package name. A Go program starts running in package main.
 import (
-  fmt
+	"fmt"
 )
 
 func strlen(s string, c chan int) { // Defines a function named strlen which takes a string s and a channel c as arguments
@@ -17,8 +17,9 @@ func strlen(s string, c chan int) { // Defines a function named strlen which tak
 }
 
 func main() { // Defines the main function
-  c := make(chan int) // Creates a channel named c of type int
+	c := make(chan int)         // Creates a channel named c of type int
 	go strlen("Salutations", c) // Launches the strlen function in a separate goroutine, passing "Salutations" and the channel c
-	go strlen("World", c) // Launches the strlen function in a separate goroutine, passing "World" and the channel c
-	x, y := <-c, <-c // Receives values from the channel c into variables x and y
-	fmt.Println(x, y, x+y) // Prints the values of x, y, and their sum
+	go strlen("World", c)       // Launches the strlen function in a separate goroutine, passing "World" and the channel c
+	x, y := <-c, <-c            // Receives values from the channel c into variables x and y
+	fmt.Println(x, y, x+y)      // Prints the values of x, y, and their sum
+}
